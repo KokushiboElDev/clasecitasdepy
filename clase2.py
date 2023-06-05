1429c3491338394d999a7419c5a34fdec549c9f7
Kokushibo <133360436+KokushiboElDev@users.noreply.github.com>
2023-06-05 17:31:41 -0400

    holi

--- clase2.py
+++ clase2.py
@@ -1,1 +1,11 @@
 # recordatorio subir esto a github
+
+import numpy as np
+
+lista = [1,2,3,4,5]
+arreglo = np.array(lista)
+print (arreglo)
+
+print(f"el arreglo es de {arreglo.ndim} dimension")
+
+print(f"El arreglo es de largo {len(arreglo)}")
