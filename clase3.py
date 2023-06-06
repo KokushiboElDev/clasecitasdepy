db36b1880cbb02f2da5cf92ccc3e5c2a0eb30bfc
kokushiboeldev <kokushiboeldev@gmail.com>
2023-06-06 18:24:49 -0400

    Subidon de archivos los cuales permiten mayor aprendizaje de py.

--- clase3.py
+++ clase3.py
@@ -1,1 +1,24 @@
 #recordatorio subir todo esto dentro de github
+
+
+# matriz 3x3 elemntos tipo enteros y numerico.
+# matriz 1
+# matriz 2 
+# matriz resultante multiplicacion 1 y 2
+""""""
+
+import numpy as np
+import random
+
+arreglo = np.arange(1,41)
+arreglo = arreglo.reshape(10,4)
+
+#print(arreglo)
+
+print(arreglo)
+
+# ejercicio numero 2 completado
+
+
+# ejercicio numero 3:
+
---
+++ registro nuevo de pruebas diferentes con ui y gui/intentodeprojecto.py
@@ -0,0 +1,20 @@
+"""
+Here's a project idea that uses the PyQT5 library:
+
+Project idea - Weather App using PyQT5
+
+Create a desktop weather app using PyQT5 
+that displays the current weather conditions for a given location (using an API such as OpenWeatherMap).
+
+The app should include the following features:
+
+A text input field where the user can enter the location (e.g. "London" or "New York")
+A "Search" button that triggers a request to the OpenWeatherMap API to fetch the current weather conditions for the specified location
+Display of the current weather conditions (e.g. "Mostly Cloudy, 15°C")
+A weather icon representing the current conditions (e.g. a cloud or a sun)
+Additional information such as wind speed and humidity
+Error handling for cases where the location cannot be found or the API request fails
+To start with, you can create a simple PyQT5 UI with the input field 
+button and labels to display the weather information. 
+You can then use PyQT5 signals to connect the button click event to the API request and update the UI with the fetched information.
+"""
